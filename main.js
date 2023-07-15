d41d13d45e30087e6f0ecfaf7a01c5a0e101a461
Fake Jish <pianoman32d3@gmail.com>
2023-07-15 13:10:50 -0400

    changing port to environment variable port

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log("listening at 3000"));
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
-require('dotenv').config();
+
 
 // Send the .env file over to index.html so firebase can use api keys
 app.get('/env', (req, res) => {
         res.json(process.env);
       });
-      
+
 //console.log(process.env);
 
 
